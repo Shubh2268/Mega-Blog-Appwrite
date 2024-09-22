18e7bff1464ea4b22313a79eca2902b715e88411
Shubh <shubh2268@gmail.com>
2024-09-22 17:48:34 +0530

    --

---
+++ src/appwrite/auth.js
@@ -0,0 +1,18 @@
+import config from '../config.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
