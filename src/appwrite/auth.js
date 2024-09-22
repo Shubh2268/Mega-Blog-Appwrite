7eab2a676a3f4d135cffe5ef44f712af04e4741c
Shubh <shubh2268@gmail.com>
2024-09-22 20:37:06 +0530

    --

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -9,7 +9,30 @@ export class AuthService {
     constructor() {
         this.client
             .setEndpoint(config.appwriteUrl)
-            .setProject(config.appwriteProjectId)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({name, email, password}) {
+        try {
+            const userAccount =  await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // call another method 
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+            
+        } catch (error) {
+            throw error
+        }
     }
 }
 
