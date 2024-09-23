2030424b95874ee4ee0edcc54f14ef8e97ce7175
Shubh <shubh2268@gmail.com>
2024-09-23 16:56:53 +0530

    --

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -18,6 +18,7 @@ export class AuthService {
             const userAccount =  await this.account.create(ID.unique(), email, password, name)
             if (userAccount) {
                 // call another method 
+                return this.login({email, password});
             } else {
                 return userAccount;
             }
@@ -31,7 +32,25 @@ export class AuthService {
             return await this.account.createEmailSession(email, password);
             
         } catch (error) {
-            throw error
+            throw error;
+        }
+    };
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log('Appwrite Service :: getCurrentUser :: error', error);  
+        }
+
+        return null;
+    };
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log('Appwrite Service :: logout :: error', error); 
         }
     }
 }
