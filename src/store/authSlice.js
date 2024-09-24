b5d13b1acbe7578948d6f0b37971c0450ee106c3
Shubh <shubh2268@gmail.com>
2024-09-24 17:52:18 +0530

    --

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -12,6 +12,10 @@ const authSlice = createSlice({
         login: (state, action) => {
             state.status = true;
             state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
         }
     }
 });
