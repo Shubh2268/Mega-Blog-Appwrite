dcd956c230e5b60db26a4a4e8902007d1f424d80
Shubh <shubh2268@gmail.com>
2024-09-26 17:50:38 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <div className='w-full block'>
           <Header />
           <main>
-            {/* TO DO <Outlet /> */}
+            TO DO  <Outlet />
           </main>
           <Footer />
         </div>
