b351be007d8887a5cd03afdc803a5457d343a657
Shubh <shubh2268@gmail.com>
2024-09-28 17:49:09 +0530

    --

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useId } from 'react';
 
-const Input = () => {
+const Input = React.forwardRef(({ label, type='text', className='', ...props }, ref) => {
+
+    const id = useId();
+
   return (
-    <div>
-      Input
+    <div className="w-full">
+        
     </div>
   )
-}
+})
 
-export default Input
+export default Input;
