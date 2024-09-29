caf9be941f6510d23ca389da45df3a7442e09749
Shubh <shubh2268@gmail.com>
2024-09-29 20:35:17 +0530

    --

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login as authLogin} from '../store/authSlice';
+import { Button, Input, Logo} from './index';
+import { useDispatch } from 'react-redux';
+import authService from '../appwrite/auth';
+import { useForm } from 'react-hook-form';
+
 
 const Login = () => {
   return (
@@ -8,4 +15,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
