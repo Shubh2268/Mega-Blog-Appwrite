e2a69acc57a41e701d4570ec604174ae85408cee
Shubh <shubh2268@gmail.com>
2024-09-30 20:08:21 +0530

    --

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,6 +36,11 @@ const Login = () => {
             <Logo width='100%' />
           </span>
         </div>
+        <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
+        <p className='mt-2 text-center text-base text-black/60'>
+          Don't have any account?&nbsp;
+          <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>Sign Up</Link>
+        </p>
       </div>
     </div>
   )
