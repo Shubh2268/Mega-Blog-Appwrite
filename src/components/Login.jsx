de39b9d12279b1510371a43924765dfe1f699d8c
Shubh <shubh2268@gmail.com>
2024-10-01 17:01:32 +0530

    --

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -45,15 +45,20 @@ const Login = () => {
         {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
 
         <form onSubmit={handleSubmit(login)} className='mt-8'>
-            <div className='space-y-5'>
-              <Input label='Email: ' placeholder='Enter your email' type='email' 
+          <div className='space-y-5'>
+            <Input label='Email: ' placeholder='Enter your email' type='email'
               {...register('email', {
-                required:true,
+                required: true,
                 validate: {
                   matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || 'Email address must be a valid address'
                 }
               })} />
-            </div>
+
+            <Input label='Password: ' type='password' placeholder='Enter your password'
+              {...register('password', {
+                required: true,
+              })} />
+          </div>
         </form>
       </div>
     </div>
