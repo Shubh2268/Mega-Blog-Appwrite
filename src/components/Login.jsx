1940b73bd1d7b283f2efc07fb3b8925fa04e4166
Shubh <shubh2268@gmail.com>
2024-10-01 16:58:42 +0530

    --

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,6 +41,20 @@ const Login = () => {
           Don't have any account?&nbsp;
           <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>Sign Up</Link>
         </p>
+
+        {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+
+        <form onSubmit={handleSubmit(login)} className='mt-8'>
+            <div className='space-y-5'>
+              <Input label='Email: ' placeholder='Enter your email' type='email' 
+              {...register('email', {
+                required:true,
+                validate: {
+                  matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || 'Email address must be a valid address'
+                }
+              })} />
+            </div>
+        </form>
       </div>
     </div>
   )
