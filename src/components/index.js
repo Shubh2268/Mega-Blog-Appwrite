a60bc25d8ef2f7acd83c950b5b17da8ac59d2421
Shubh <shubh2268@gmail.com>
2024-10-04 20:06:00 +0530

    --

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,17 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from './container/Container';
-import Logo from './Logo';
-import LogoutBtn from './Header/LogoutBtn';
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Select from "./Select";
 import Signup from "./Signup";
 import Login from "./Login";
+import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
-
-
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
@@ -18,9 +19,12 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    Select,
     Signup,
     Login,
+    Button,
     PostForm,
-    PostCard
-};
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
