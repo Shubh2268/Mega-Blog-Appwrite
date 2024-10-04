520d032b75109c4bf8d9cc63513c3855e53bc0a6
Shubh <shubh2268@gmail.com>
2024-10-04 17:08:01 +0530

    --

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, PostForm } from '../components';
 import appwriteService from '../appwrite/config';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const EditPost = () => {
+
+  const [post, setPosts] = useState(null);
+  const {slug} = useParams();
+  const navigate = useNavigate();
+
   return (
     <div>
       
