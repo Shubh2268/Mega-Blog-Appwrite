a5632163be4dbd85b7128db9592d0a13bf018c87
Shubh <shubh2268@gmail.com>
2024-10-04 17:28:50 +0530

    --

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,11 +14,21 @@ const Home = () => {
         })
     }, []);
 
-    return (
-        <div>
-
-        </div>
-    )
+    if (posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
 }
 
 export default Home;
